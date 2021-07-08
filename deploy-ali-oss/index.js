d6b6c22d96c0293285af6d1e66fa0c926c995b45
赵东升 <1197160272@qq.com>
2021-07-08 16:34:15 +0800

    上传至阿里OSS改为依赖包处理

--- deploy-ali-oss/index.js
+++ deploy-ali-oss/index.js
@@ -1,41 +1,9 @@
-const OSS = require('ali-oss')
-const path = require('path')
-const getUploadFiles = require('./getUploadFiles')
+const deploy = require('ali-oss-static-web-deploy')
 
-const aliOssClient = new OSS({
+deploy({
   region: 'oss-cn-beijing',
   accessKeyId: 'LTAIw3zvXKJhMlZ7',
   accessKeySecret: 'eUKPy2NsCB4KUgafGQSeMIoyWbiRhU',
-  bucket: 'donsen-blog'
+  bucket: 'donsen-blog',
+  staticWebAppPath: require('path').resolve(process.cwd(), 'output')
 })
-
-const getAllBucketFiles = async() => {
-  const allFiles = []
-  let marker = null
-  do {
-    const res = await aliOssClient.list({
-      marker
-    })
-    marker = res.nextMarker
-    if (res.objects) {
-      allFiles.push(...res.objects)
-    }
-  } while (marker)
-  return new Promise(resolve => {
-    resolve(allFiles)
-  })
-}
-
-const deleteThenUpload = async() => {
-  const oldFiles = await getAllBucketFiles()
-  if (oldFiles && oldFiles.length > 0) {
-    await aliOssClient.deleteMulti(oldFiles.map(({ name }) => name), { quiet: true })
-  }
-  // 上传所有打包结果文件
-  const newFiles = await getUploadFiles()
-  newFiles.forEach(async([localPath, remotePath]) => {
-    await aliOssClient.put(remotePath, path.normalize(localPath))
-  })
-}
-
-deleteThenUpload()
